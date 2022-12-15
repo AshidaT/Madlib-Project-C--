ee835ee3ba6157afd765566be1596cc9de8ce272
AshidaT <120616979+AshidaT@users.noreply.github.com>
2022-12-14 22:43:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-// Function to print the main menu
 void printMenu()
 {
   cout << "Welcome to the Madlib game!" << endl;
@@ -13,13 +12,11 @@ void printMenu()
   cout << "2. Stop game" << endl;
 }
 
-// Function to play the madlib game
 void playGame()
 {
-  // Create a vector to store the user's inputs
+
   vector<string> inputs;
 
-  // Prompt the user for inputs and store them in the vector
   cout << "Please enter a noun: ";
   string noun;
   cin >> noun;
@@ -35,33 +32,30 @@ void playGame()
   cin >> adjective;
   inputs.push_back(adjective);
 
-  // Use the user's inputs to create a madlib sentence
   cout << "The " << inputs[0] << " likes to " << inputs[1] << " in a very " << inputs[2] << " way." << endl;
 }
 
 int main()
 {
-  // Print the main menu
+
   printMenu();
 
-  // Get the user's menu selection
   int selection;
   cin >> selection;
 
-  // Handle the user's menu selection
   if (selection == 1)
   {
-    // Play the game
+
     playGame();
   }
   else if (selection == 2)
   {
-    // Quit the game
+
     cout << "Thanks for playing! Goodbye." << endl;
   }
   else
   {
-    // Invalid selection
+
     cout << "Invalid selection. Please try again." << endl;
   }
 
